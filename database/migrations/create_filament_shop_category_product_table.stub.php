29a543731e7589d2a7a19bf43bcae7463a988f7e
a21ns1g4ts <atila.danvi@outlook.com>
2025-02-20 19:28:19 -0300

    Remove redundant foreign key constraint in category-product migration

--- database/migrations/create_filament_shop_category_product_table.stub.php
+++ database/migrations/create_filament_shop_category_product_table.stub.php
@@ -11,15 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('filament_shop_category_product', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('category_id')->constrained('filament_shop_categories')->cascadeOnDelete();
-            $table->foreignId('category_id')->constrained('filament_shop_products')->cascadeOnDelete();
-            $table->timestamps();
-        });
-    }
+  
 
     /**
      * Reverse the migrations.
