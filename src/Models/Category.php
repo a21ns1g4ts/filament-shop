0c11f8cf845396be511185cb0ad7eb7ab0739945
a21ns1g4ts <atila.danvi@outlook.com>
2025-03-13 04:25:41 -0300

    Add Str facade import to Category model for slug generation

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 use RalphJSmit\Laravel\SEO\Support\HasSEO;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
